6bb2ec6fffc7d92f759e8e0122cee01755ab1814
amitshah1modi-prog <amitshah.1modi@gmail.com>
2025-12-30 15:29:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
 import SuperAdmin from "./pages/SuperAdmin";
+import Hello from "./pages/Hello";
 import AdminAgents from "./pages/AdminAgents";
 import AgentHistoryPage from "./pages/AgentHistoryPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<SuperAdmin />} />
-      <Route path="/admin/:adminId" element={<AdminAgents />} />
-      <Route
-        path="/admin/:adminId/agent/:agentId"
-        element={<AgentHistoryPage />}
-      />
+      <Route element={<Layout />}>
+        <Route path="/" element={<SuperAdmin />} />
+        <Route path="/hello" element={<Hello />} />
+        <Route path="/admin/:adminId" element={<AdminAgents />} />
+        <Route
+          path="/admin/:adminId/agent/:agentId"
+          element={<AgentHistoryPage />}
+        />
+      </Route>
     </Routes>
   );
 }
