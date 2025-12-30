28bd95b49a1d37ca64557328b5fc2e783ef12a78
amitshah1modi-prog <amitshah.1modi@gmail.com>
2025-12-30 15:24:30 +0530

    Create Hello.jsx

---
+++ src/pages/Hello.jsx
@@ -0,0 +1,14 @@
+function Hello() {
+  return (
+    <div style={{
+      fontSize: "48px",
+      fontWeight: "800",
+      textAlign: "center",
+      marginTop: "100px"
+    }}>
+      HELLO
+    </div>
+  );
+}
+
+export default Hello;
