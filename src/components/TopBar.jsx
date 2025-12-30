70e3295450b80ee1de22b68c1c4952911e1d7f7f
amitshah1modi-prog <amitshah.1modi@gmail.com>
2025-12-30 15:19:30 +0530

    Create TopBar.jsx

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,18 @@
+function TopBar() {
+  return (
+    <div style={{
+      height: "60px",
+      backgroundColor: "#0f172a",
+      color: "white",
+      display: "flex",
+      alignItems: "center",
+      padding: "0 20px",
+      fontSize: "18px",
+      fontWeight: "bold"
+    }}>
+      Super Admin Panel
+    </div>
+  );
+}
+
+export default TopBar;
