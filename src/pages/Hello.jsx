36bb2b176c373fb21c2c89ab8dd9922157f1d2eb
amitshah1modi-prog <amitshah.1modi@gmail.com>
2025-12-30 15:28:22 +0530

    Update Hello.jsx

--- src/pages/Hello.jsx
+++ src/pages/Hello.jsx
@@ -1,12 +1,14 @@
 function Hello() {
   return (
-    <div style={{
-      fontSize: "48px",
-      fontWeight: "800",
-      textAlign: "center",
-      marginTop: "100px"
-    }}>
-      HELLO
+    <div
+      style={{
+        fontSize: "64px",
+        fontWeight: "800",
+        textAlign: "center",
+        marginTop: "100px",
+      }}
+    >
+      HELLO 👋
     </div>
   );
 }
