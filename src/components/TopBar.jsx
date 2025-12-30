9380869ba27c8c3588646a6ec7ec7ab219928112
amitshah1modi-prog <amitshah.1modi@gmail.com>
2025-12-30 15:27:18 +0530

    Update TopBar.jsx

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,15 +1,17 @@
 function TopBar() {
   return (
-    <div style={{
-      height: "60px",
-      backgroundColor: "#0f172a",
-      color: "white",
-      display: "flex",
-      alignItems: "center",
-      padding: "0 20px",
-      fontSize: "18px",
-      fontWeight: "bold"
-    }}>
+    <div
+      style={{
+        height: "60px",
+        background: "#0f172a",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        padding: "0 20px",
+        fontSize: "18px",
+        fontWeight: "600",
+      }}
+    >
       Super Admin Panel
     </div>
   );
