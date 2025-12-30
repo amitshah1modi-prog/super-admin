7df573227f41c6ae72991c54d1e516d638d9275b
amitshah1modi-prog <amitshah.1modi@gmail.com>
2025-12-30 11:12:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import SuperAdmin from "./pages/SuperAdmin";
+import AdminAgents from "./pages/AdminAgents";
 
 function App() {
-  return <SuperAdmin />;
+  return (
+    <Routes>
+      <Route path="/" element={<SuperAdmin />} />
+      <Route path="/admin/:adminId" element={<AdminAgents />} />
+    </Routes>
+  );
 }
 
 export default App;
