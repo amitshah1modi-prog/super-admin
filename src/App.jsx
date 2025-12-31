27535ae86195b53c0cede88b1e52c38c64b078d2
amitshah1modi-prog <amitshah.1modi@gmail.com>
2025-12-31 11:16:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
+
 import SuperAdmin from "./pages/SuperAdmin";
 import Hello from "./pages/Hello";
 import AdminAgents from "./pages/AdminAgents";
 import AgentHistoryPage from "./pages/AgentHistoryPage";
 import AdminAnalysis from "./pages/AdminAnalysis";
 import AdminReport from "./pages/AdminReport";
+import AgentWiseAnalysis from "./pages/AgentWiseAnalysis";
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
+        {/* MAIN PAGES */}
         <Route path="/" element={<SuperAdmin />} />
         <Route path="/hello" element={<Hello />} />
+
+        {/* ADMIN ANALYSIS */}
         <Route path="/analysis" element={<AdminAnalysis />} />
         <Route path="/admin-report" element={<AdminReport />} />
 
+        {/* AGENT WISE ANALYSIS */}
+        <Route path="/agent-analysis" element={<AgentWiseAnalysis />} />
+
+        {/* ADMIN → AGENTS FLOW */}
         <Route path="/admin/:adminId" element={<AdminAgents />} />
         <Route
           path="/admin/:adminId/agent/:agentId"
@@ -27,4 +36,3 @@ function App() {
 }
 
 export default App;
-
